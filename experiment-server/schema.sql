a909f41f5f2026ad819637f307b2048ba418952e
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-05-26 17:11:55 -0500

    Forgot to commit the updated schema.

--- experiment-server/schema.sql
+++ experiment-server/schema.sql
@@ -3,15 +3,15 @@ CREATE TABLE IF NOT EXISTS trajectories(
   start_state BLOB NOT NULL,
   actions BLOB NOT NULL,
   length INT NOT NULL,
-  env TEXT NOT NULL,
-  modality TEXT NOT NULL
+  env TEXT NOT NULL
+  modality TEXT NOT NULL,
 );
 CREATE TABLE IF NOT EXISTS questions(
   id INTEGER PRIMARY KEY,
   first_id INT NOT NULL,
   second_id INT NOT NULL,
-  modality TEXT NOT NULL,
-  algorithm TEXT NOT NULL
+  algorithm TEXT NOT NULL,
+  env TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS users(
   id INTEGER PRIMARY KEY,
