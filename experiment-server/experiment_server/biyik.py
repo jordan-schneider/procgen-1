Fixes minor bugs in test + min distance calculation.

experiment-server/experiment_server/biyik.py
@@ -147,12 +147,12 @@ def successive_elimination(
 
     while len(indices) > n_out_questions:
         mins = np.where(dists == np.min(dists))
+        assert np.array_equal(np.sort(mins[0]), np.sort(mins[1])), f"Minimum distances not symmetric. mins={mins}, dists={dists}"
         assert (
-            len(mins) > 1 and len(mins[0]) == 2
-        ), "Distance matrix is symmetric, there should be at least two minimums"
-        min_index = mins[0]
+            len(mins[0]) >= 1
+        ), f"There should be at least two minimums. mins={mins}, dists={dists}"
+        delete_dist_index = mins[0][0]
 
-        delete_dist_index = min_index[np.argmin(infogains[indices][min_index])]
         dists = np.delete(dists, delete_dist_index, axis=0)
         dists = np.delete(dists, delete_dist_index, axis=1)
 

experiment-server/tests/test_utils.py
@@ -16,6 +16,13 @@ nonzero_arrays = arrays(dtype=np.float32, shape=(5, 4), elements=floats_1000).fi
 )
 
 
+def min_magnitude(vecs: np.ndarray) -> float:
+    tmp_vecs = np.copy(vecs)
+    tmp_vecs[tmp_vecs == 0.0] = np.max(np.abs(tmp_vecs))
+    sensitivity = np.min(np.abs(tmp_vecs)) / 2
+    return sensitivity
+
+
 @composite
 def arrays_insert(draw, arrays):
     vecs = draw(arrays)
@@ -25,19 +32,15 @@ def arrays_insert(draw, arrays):
 
 @given(vecs=unique_arrays)
 def test_remove_duplicates_noop(vecs: np.ndarray):
-    assert np.array_equal(vecs, remove_duplicates(vecs)[0])
+    sensitivity = min_magnitude(vecs)
+    assert np.array_equal(vecs, remove_duplicates(vecs, sensitivity)[0])
 
 
 @given(vecs_and_insert=arrays_insert(arrays=unique_arrays), n_duplicates=integers(1, 5))
 def test_remove_duplicates(vecs_and_insert: Tuple[np.ndarray, int], n_duplicates: int):
     vecs, start_index = vecs_and_insert
     dup_vecs = np.insert(vecs, start_index, np.tile(vecs[0], (n_duplicates, 1)), axis=0)
-    # Get the smallest exponent of any value
-    tmp_vecs = np.copy(vecs)
-    tmp_vecs[tmp_vecs == 0.0] = np.max(np.abs(tmp_vecs))
-    sensitivity = np.min(np.abs(tmp_vecs)) / 2
-
-    out_vecs, indices = remove_duplicates(dup_vecs, sensitivity)
+    out_vecs, indices = remove_duplicates(dup_vecs, min_magnitude(vecs))
     assert np.array_equal(dup_vecs[np.sort(indices)], vecs)
 
 