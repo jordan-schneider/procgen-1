9e93c6db4fb7257ed14456c4f9187992a35e00dd
Jordan Schneider <jordan.jack.schneider@gmail.com>
2022-06-24 17:07:34 -0500

    Moves some root level heroku config into experiment server repository directly.

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 ./procgen/build.sh
-cp procgen/jsbuild/dist/env.js experiment-server/experiment_server/static/
-cp -r procgen/jsbuild/dist/assets/* experiment-server/experiment_server/static/
-# Realistically I need multiple pages, there's nothing super interesting in this html file, it just
-# gets copied over a bunch.
-# cp procgen/jsbuild/dist/index.html experiment-server/experiment_server/templates/
+cp procgen/jsbuild/dist/env.js ~/experiment-server/experiment_server/static/
+cp -r procgen/jsbuild/dist/assets/* ~/experiment-server/experiment_server/static/
